aa4a5856f94fb6ccbf880c63e7c3fe446a43640a
ThunderTecke <thunder.tecke@gmail.com>
2023-03-04 18:51:35 -0500

    SetupTool class creation

--- PID_Py/SetupTool.py
+++ PID_Py/SetupTool.py
@@ -1,7 +1,28 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6 import QtGui
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
+from PySide6.QtWidgets import QVBoxLayout, QHBoxLayout
 
-app = QApplication(sys.argv)
-label = QLabel("Hello world")
-label.show()
-app.exec()
+class SetupToolApp(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.setWindowTitle("PID_Py : SetupTool")
+        self.setFixedSize(1000, 600)
+
+        # ===== Real-time graph ====
+        
+
+        # ===== Central widget =====
+        centralWidget = QWidget()
+        centralLayout = QVBoxLayout(centralWidget)
+
+        self.setCentralWidget(centralWidget)
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+
+    setupToolApp = SetupToolApp()
+    setupToolApp.show()
+
+    app.exec()
